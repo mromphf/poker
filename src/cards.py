ace0ed83efc1cd1dd7402d3978a4ee70120a9f98
Mark Romphf <markromphf@gmail.com>
2026-05-02 14:10:18 -0600

    Docstrings for Cards, Ranks, and Suits.

--- src/cards.py
+++ src/cards.py
@@ -1,9 +1,23 @@
+"""
+Provides enums for Rank and Suit, combined
+to form the Card class.
+"""
+
 from dataclasses import dataclass
 from enum import Enum
 from functools import total_ordering
 
 
 class Suit(Enum):
+    """
+    A classic french deck has four colored suits:
+
+    Hearts, Diamonds, Spades, Clubs
+
+    Hearts and Diamonds are Red, Spades and
+    Clubs are Black. There is no standard order for Suits.
+    """
+
     HEARTS = 1
     CLUBS = 2
     DIAMONDS = 3
@@ -12,9 +26,28 @@ class Suit(Enum):
     def __str__(self):
         return str(self.name)
 
+    def __repr__(self):
+        return self.__str__()
+
 
 @total_ordering
 class Rank(Enum):
+    """
+    A classic french deck contains four sets
+    of 13 ranks from Ace to King.
+
+    Aces can traditionally be low or high depending
+    on the game or use case. This enum models Aces
+    as the LOWEST rank: 1
+
+    Jack, Queen, and King are called "Face" cards
+    and are ranked 11, 12, and 13 respectively. Ace
+    High ranks higher than a King.
+
+    Adding Ranks yields an integer based on the
+    sum of their values.
+    """
+
     ACE = 1
     TWO = 2
     THREE = 3
@@ -31,6 +64,9 @@ class Rank(Enum):
 
     def __str__(self):
         return str(self.name)
+
+    def __repr__(self):
+        return self.__str__()
 
     def __gt__(self, other):
         return self.value > other.value
@@ -57,6 +93,15 @@ class Rank(Enum):
 @dataclass
 @total_ordering
 class Card:
+    """
+    A Card is a combination of a Rank and a Suit.
+
+    Adding cards together yields the sum of their ranks.
+
+    Sorting cards orders by rank, with Aces considered
+    low.
+    """
+
     rank: Rank
     suit: Suit
 
